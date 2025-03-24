ef6d05c40085eaed152cf222f64ecc5fbfa2158c
IvanilovA147 <alina.o.ivanilova@ukd.edu.ua>
2025-03-24 11:01:51 +0200

    9 pr

--- Project_Django/project_Django/polls/admin.py
+++ Project_Django/project_Django/polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from polls.models import Question
+
+
 # Register your models here.
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
--- Project_Django/project_Django/project_Django/urls.py
+++ Project_Django/project_Django/project_Django/urls.py
@@ -1,3 +1,6 @@
+
+
+
 """
 URL configuration for project_Django project.
 
@@ -14,9 +17,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
 ]
