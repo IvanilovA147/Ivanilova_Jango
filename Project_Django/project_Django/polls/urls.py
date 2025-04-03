975843057bf69a69dfc069df1015bc020419c86f
IvanilovA147 <alina.o.ivanilova@ukd.edu.ua>
2025-04-03 09:51:11 +0300

    ALILYA

--- Project_Django/project_Django/polls/urls.py
+++ Project_Django/project_Django/polls/urls.py
@@ -1,7 +1,8 @@
+
 from django.urls import path
 from . import views
 
-app_name = 'polls'
 urlpatterns = [
-    path('', views.question_list, name='question_list'),
+    path('register/', views.register_view, name='register'),
+    path("", views.question_list, name= "question_list")
 ]
--- Project_Django/project_Django/polls/views.py
+++ Project_Django/project_Django/polls/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
-
 # Create your views here.
 
-
-from django.shortcuts import render
 from .models import Question
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegistrationForm
+
 
 def question_list(request):
+    # Отримуємо всі питання з моделі Question
     questions = Question.objects.all()
     context = {'questions': questions}
-    return render(request, 'Яquestion_list.html', context)
+    return render(request, 'question_list.html', context)
+
+def register_view(request):
+    if request.method == "POST":
+        # Створення форми з переданими даними POST
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            # Збереження нового користувача та входження в систему
+            user = form.save()
+            login(request, user)
+            # Перенаправлення на домашню сторінку після успішної реєстрації
+            return redirect('http://127.0.0.1:8000/polls/')
+    else:
+        # Якщо метод не POST, то створюємо порожню форму
+        form = RegistrationForm()
+
+    # Повертаємо форму в шаблон
+    return render(request, 'register.html', {"form": form})
