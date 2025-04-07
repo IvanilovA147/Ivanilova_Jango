f500877dfcfd3193b6e5db09862668be630fe0d2
IvanilovA147 <alina.o.ivanilova@ukd.edu.ua>
2025-04-07 10:50:18 +0300

    10.2

--- Project_Django/project_Django/polls/urls.py
+++ Project_Django/project_Django/polls/urls.py
@@ -2,7 +2,11 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'polls'
 urlpatterns = [
     path('register/', views.register_view, name='register'),
-    path("", views.question_list, name= "question_list")
+    path("", views.question_list, name= "question_list"),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- Project_Django/project_Django/polls/views.py
+++ Project_Django/project_Django/polls/views.py
@@ -2,8 +2,10 @@
 
 from .models import Question
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from .forms import RegistrationForm
+from django.contrib.auth import login, logout, authenticate
+from .forms import RegistrationForm, UserLoginForm
+
+
 
 
 def question_list(request):
@@ -28,3 +30,53 @@ def register_view(request):
 
     # Повертаємо форму в шаблон
     return render(request, 'register.html', {"form": form})
+
+
+# views.py
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from .forms import UserLoginForm
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # або будь-яка інша сторінка
+    else:
+        form = UserLoginForm()
+
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # або 'home'
+
+
+# views.py
+def login_view(request):
+    if request.method == "POST":
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  # або будь-яка інша сторінка
+    else:
+        form = UserLoginForm()
+
+    return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')  # або 'home'
